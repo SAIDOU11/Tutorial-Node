7d8feedacefb51086e86d982c8698ee1fc0eba36
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-01 16:01:12 +0200

    sync vs async

---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+// CommonJS everyfile in Node is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+
+const names = require('./4-names.js');
+const sayHi = require('./5-utils.js');
+const data = require('./6-alternative-flavor.js');
+require('./7-mind-grenade.js');
+
+sayHi('Susan');
+sayHi(names.bob);
+sayHi(names.john);
