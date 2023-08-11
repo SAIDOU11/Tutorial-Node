a65df596034a970c2dad9b5a31404b52c64a4449
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-11 15:39:08 +0200

    await

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-const http = require('http');
+const EventEmitter = require('events');
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('HOME PAGE');
-  }
-  if (req.url === '/about') {
-    // BLOCKING CODE !!!
-    // for (let i = 0; i < 1000; i++) {
-    //   for (let j = 0; j < 1000; j++) {
-    //     console.log(`${i} ${j}`);
-    //   }
-    // }
-    res.end('ABOUT PAGE');
-  }
-  res.end('ERROR PAGE');
-});
-
-server.listen(5000, () => {
-  console.log('Server is listening on port 5000');
-});
+const customEmitter = new EventEmitter();
